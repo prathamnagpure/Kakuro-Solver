65772178ebdb22bbf19b3b916fc2a1254d22fa96
prathamnagpure <prathamnagpure2002@gmail.com>
2022-07-29 21:20:35 +0530

    modified code/BS-AC3.py code/BS-MAC.py code/BS.py

--- code/BS-AC3.py
+++ code/BS-AC3.py
@@ -1,94 +1,86 @@
 from itertools import permutations
 
-backtracking_count = 0
-consistency_checks_by_backtracking = 0
-
 #class to define the csp
-class CSP:
-    def __init__(self, variables, domains, neighbours, constraints):
+class KakuroCSP:
+    def __init__(self, variables, domain, neighbours, constraints):
         self.variables = variables
         self.domain = domain
         self.neighbours = neighbours
         self.constraints = constraints
         
-#returns the number found in a line skipping other characters
-def get_decimal_number(line):
-    number = 0
-    for z in line:
-        if z.isdigit():
-            number = number*10 + int(z)
-    return number
-    
-#returns the domain values of a variable
-def order_domain_values(csp, variable):
-    return csp.domain[variable]
-    
-#if assigning the value to variable is consistent with previous assignments
-#returns True
-#else return False
-def consistent(csp, variable, value, assignment):
-    if variable[0] == "U":
-        for neighbour in csp.neighbours[variable]:
-            if neighbour in assignment:
-                if assignment[neighbour] != value[csp.constraints[variable+neighbour]]:
-                    return False
-        return True
-    else:
-        for neighbour in csp.neighbours[variable]:
-            if neighbour in assignment:
-                if assignment[neighbour][csp.constraints[neighbour+variable]] != value:
-                    return False
-        return True
+    #returns the domain values of a variable
+    def order_domain_values(self, variable):
+        return self.domain[variable]
+ 
+    #if assigning the value to variable is consistent with previous assignments
+    #returns True
+    #else return False
+    def consistent(self, variable, value, assignment):
+    #checking if the variable is a empty cell variable or the one assigned with constraints
+        if variable[0] == "U":
+            for neighbour in self.neighbours[variable]:
+                if neighbour in assignment:
+                    if assignment[neighbour] != value[self.constraints[variable+neighbour]]:
+                        return False
+            return True
+        else:
+            for neighbour in self.neighbours[variable]:
+                if neighbour in assignment:
+                    if assignment[neighbour][self.constraints[neighbour+variable]] != value:
+                        return False
+            return True
 
-            
-def remove_inconsistent_values(csp, xi, xj):
-    removed = False
-    for valueXi in csp.domain[xi]:
-        remove = True
-        for valueXj in csp.domain[xj]:
-            if xj[0] == "U":
-                if valueXi == valueXj[csp.constraints[xj+xi]]:
-                    remove = False
-                    break
-            else:
-                if valueXi[csp.constraints[xi+xj]] == valueXj:
-                    remove = False
-                    break
-        if remove:
-            csp.domain[xi].remove(valueXi)
-            removed = True
-    return removed
+    #removes the inconsistent values from the domain and prunes the domain
+    #returns true if a value is removed
+    def remove_inconsistent_values(self, xi, xj):
+        removed = False
+        for valueXi in self.domain[xi]:
+            remove = True
+            for valueXj in self.domain[xj]:
+                if xj[0] == "U":
+                    if valueXi == valueXj[self.constraints[xj+xi]]:
+                        remove = False
+                        break
+                else:
+                    if valueXi[self.constraints[xi+xj]] == valueXj:
+                        remove = False
+                        break
+            if remove:
+                self.domain[xi].remove(valueXi)
+                removed = True
+        return removed
+    #finds a variable which is not assigned and returns it
+    def select_unassigned_variable(self, assignment):
+        for variable in self.variables:
+            if variable not in assignment:
+                return variable
 
+#The AC-3 algorithm returns false if the domain becomes empty of some variable
 def AC_3(csp, queue=None):
     if queue == None:
         queue = [(xi,xj) for xi in csp.variables for xj in csp.neighbours[xi]]
     while queue:
         xi,xj = queue.pop(0)
-        removed = remove_inconsistent_values(csp, xi, xj)
+        removed = csp.remove_inconsistent_values(xi, xj)
         if removed:
             if len(csp.domain[xi]) == 0:
                 return False
+            #add the neighbour arcs in queue if some value is removed
             for neighbour in csp.neighbours[xi]:
                 queue.append((neighbour, xi))
     return True
-    
-#finds a variable which is not assigned and returns it
-def select_unassigned_variable(csp, assignment):
-    for variable in csp.variables:
-        if variable not in assignment:
-            return variable
-
+                
 #recursive backtracking which returns solution or None
 def recursive_backtracking(csp,assignment):
-    global backtracking_count
-    global consistency_checks_by_backtracking
-    backtracking_count += 1
+    #global backtracking_count
+    #global consistency_checks_by_backtracking
+    #backtracking_count += 1
     if len(assignment) == len(csp.variables):
         return assignment
-    variable = select_unassigned_variable(csp,assignment)
-    for value in order_domain_values(csp, variable):
-        consistency_checks_by_backtracking += 1
-        if consistent(csp, variable, value, assignment):
+    variable = csp.select_unassigned_variable(assignment)
+    for value in csp.order_domain_values(variable):
+        #consistency_checks_by_backtracking += 1
+        if csp.consistent(variable, value, assignment):
             assignment[variable] = value
             result = recursive_backtracking(csp,assignment)
             if result != None:
@@ -99,124 +91,137 @@ def recursive_backtracking(csp,assignment):
 
 def backtracking_search(csp):
     return recursive_backtracking(csp,{})
+
+#returns the number found in a line skipping other characters
+def get_decimal_number(line):
+    number = 0
+    for z in line:
+        if z.isdigit():
+            number = number*10 + int(z)
+    return number
     
-#Taking the input
-file_name = input()
-file_object = open(file_name, "r")
-file_data = file_object.read().splitlines()
-file_object.close()
-get_decimal_number(file_data[0])
-file_data = [i for i in file_data if i]
-rows = get_decimal_number(file_data[0])
-columns = get_decimal_number(file_data[1])
-horizontal = [file_data[i].split(",") for i in range(3,3+rows)]
-vertical = [file_data[i].split(",") for i in range(4+rows, 4+rows+rows)]
-variables = []
-domain = {}
-constraints = {}
-neighbours = {}
-default_domain = [i for i in range(1,10)]
-sum_constraint_found = False
-sum_value = 0
-
-#For the horizontal values
-for i in range(rows):
-    for j in range(columns):
-        #Find sum clues
-        if horizontal[i][j] != "#" and horizontal[i][j] != "0":
-            u_variable_name = "UR"+str(i)+","+str(j)
-            #Add the variable
-            variables.append(u_variable_name)
-            neighbours[u_variable_name] = []
-            k = j + 1
-            ct = 0
-            #set the domains constraints neighbours variables of the block
-            while k < columns:
-                if horizontal[i][k] == "0":
-                    x_variable_name = "X"+str(i)+","+str(k)
-                    variables.append(x_variable_name)
-                    neighbours[x_variable_name] = []
-                    neighbours[x_variable_name].append(u_variable_name)
-                    neighbours[u_variable_name].append(x_variable_name)
-                    domain[x_variable_name] = [l for l in range(1,min(int(horizontal[i][j]),10))]
-                    constraints[u_variable_name+x_variable_name] = ct
-                    ct += 1
-                else:
-                    break
-                k += 1
-            domain[u_variable_name] = [lst for lst in permutations([1,2,3,4,5,6,7,8,9],k-j-1) if sum(lst) == int(horizontal[i][j])]
-            j = k
-
-#For the vertical values
-for j in range(columns):
-    for i in range(rows):
-        #find the sum clues
-        if vertical[i][j] != "#" and vertical[i][j] != "0":
-            u_variable_name = "UD"+str(i)+","+str(j)
-            #Add the variable
-            variables.append(u_variable_name)
-            neighbours[u_variable_name] = []
-            k = i + 1
-            ct = 0
-            #set the domains constraints neighbours variables in block
-            while k < rows:
-                if vertical[k][j] == "0":
-                    x_variable_name = "X"+str(k)+","+str(j)
-                    if x_variable_name not in variables:
+def make_kakuro_csp(number_of_rows,number_of_columns,horizontal,vertical):
+    variables = []
+    domain = {}
+    constraints = {}
+    neighbours = {}
+    
+    #For the horizontal values
+    for i in range(number_of_rows):
+        for j in range(number_of_columns):
+            #Find sum clues
+            if horizontal[i][j] != "#" and horizontal[i][j] != "0":
+                u_variable_name = "UR" + str(i) + "," + str(j)
+                #Add the variable
+                variables.append(u_variable_name)
+                neighbours[u_variable_name] = []
+                k = j + 1
+                ct = 0
+                #set the domain, constraints, neighbours and variables of the block
+                while k < number_of_columns:
+                    if horizontal[i][k] == "0":
+                        x_variable_name = "X" + str(i) + "," + str(k)
                         variables.append(x_variable_name)
-                    if x_variable_name in neighbours:
+                        neighbours[x_variable_name] = []
                         neighbours[x_variable_name].append(u_variable_name)
+                        neighbours[u_variable_name].append(x_variable_name)
+                        domain[x_variable_name] = [l for l in range(1,min(int(horizontal[i][j]),10))]
+                        constraints[u_variable_name+x_variable_name] = ct
+                        ct += 1
                     else:
-                        neighbours[x_variable_name] = [u_variable_name]
-                    neighbours[u_variable_name].append(x_variable_name)
-                    if x_variable_name in domain:
-                        domain[x_variable_name] = list(set(domain[x_variable_name]).intersection(set([l for l in range(1,min(int(vertical[i][j]),10))])))
-                    else:
-                        domain[x_variable_name] = [l for l in range(1,min(int(vertical[i][j]),10))]
-                    constraints[u_variable_name+x_variable_name] = ct
-                    ct += 1
-                else:
-                    break
-                k += 1
-            domain[u_variable_name] = [lst for lst in permutations([1,2,3,4,5,6,7,8,9],k-i-1) if sum(lst) == int(vertical[i][j])]
-            i = k
-#set the csp
-csp = CSP(variables, domain, neighbours, constraints)
-#apply arc consistency on all arcs
-AC_3(csp)
-#Find the solution by backtracking search
-assignment = backtracking_search(csp)
+                        break
+                    k += 1
+                domain[u_variable_name] = [lst for lst in permutations([1,2,3,4,5,6,7,8,9],k-j-1) if sum(lst) == int(horizontal[i][j])]
+                j = k
 
-print("backtracking calls",backtracking_count)
-print("consistency checks by backtracking",consistency_checks_by_backtracking)
+    #For the vertical values
+    for j in range(number_of_columns):
+        for i in range(number_of_rows):
+            #find the sum clues
+            if vertical[i][j] != "#" and vertical[i][j] != "0":
+                u_variable_name = "UD"+str(i)+","+str(j)
+                #Add the variable
+                variables.append(u_variable_name)
+                neighbours[u_variable_name] = []
+                k = i + 1
+                ct = 0
+                #set the domains constraints neighbours variables in block
+                while k < number_of_rows:
+                    if vertical[k][j] == "0":
+                        x_variable_name = "X"+str(k)+","+str(j)
+                        if x_variable_name not in variables:
+                            variables.append(x_variable_name)
+                        if x_variable_name in neighbours:
+                            neighbours[x_variable_name].append(u_variable_name)
+                        else:
+                            neighbours[x_variable_name] = [u_variable_name]
+                        neighbours[u_variable_name].append(x_variable_name)
+                        if x_variable_name in domain:
+                            domain[x_variable_name] = list(set(domain[x_variable_name]).intersection(set([l for l in range(1,min(int(vertical[i][j]),10))])))
+                        else:
+                            domain[x_variable_name] = [l for l in range(1,min(int(vertical[i][j]),10))]
+                        constraints[u_variable_name+x_variable_name] = ct
+                        ct += 1
+                    else:
+                        break
+                    k += 1
+                domain[u_variable_name] = [lst for lst in permutations([1,2,3,4,5,6,7,8,9],k-i-1) if sum(lst) == int(vertical[i][j])]
+                i = k
+    #return the csp
+    return KakuroCSP(variables, domain, neighbours, constraints)
 
 #To get the outputfile named with the number of input file
-output_file_name = "output" + str(get_decimal_number(file_name)) + ".txt"
-out_file_object = open(output_file_name, 'w+')
-out_file_object.write(file_data[0] + "\n")
-out_file_object.write(file_data[1] + "\n")
-out_file_object.write(file_data[2] + "\n")
-for i in range(rows):
-    for j in range(columns):
-        if horizontal[i][j] == '#' or horizontal[i][j] != '0':
-            out_file_object.write(horizontal[i][j])
-        else:
-            out_file_object.write(str(assignment["X"+str(i)+","+str(j)]))
-        if j != columns - 1:
-                out_file_object.write(",")
-    out_file_object.write("\n")
-out_file_object.write(file_data[3+rows] + "\n")
-for i in range(rows):
-    for j in range(columns):
-        if horizontal[i][j] == '#' or horizontal[i][j] != '0':
-            out_file_object.write(vertical[i][j])
-        else:
-            out_file_object.write(str(assignment["X"+str(i)+","+str(j)]))
-        if j != columns - 1:
-                out_file_object.write(",")
-    out_file_object.write("\n")
-            
-
+def write_solution_to_output_file(output_file_name,input_file_data,horizontal,vertical,number_of_rows,number_of_columns,assignment):
+    out_file_object = open(output_file_name, 'w+')
+    out_file_object.write(input_file_data[0] + "\n")
+    out_file_object.write(input_file_data[1] + "\n")
+    out_file_object.write(input_file_data[2] + "\n")
+    for i in range(number_of_rows):
+        for j in range(number_of_columns):
+            if horizontal[i][j] == '#' or horizontal[i][j] != '0':
+                out_file_object.write(horizontal[i][j])
+            else:
+                out_file_object.write(str(assignment["X"+str(i)+","+str(j)]))
+            if j != number_of_columns - 1:
+                    out_file_object.write(",")
+        out_file_object.write("\n")
+    out_file_object.write(input_file_data[3+number_of_rows] + "\n")
+    for i in range(number_of_rows):
+        for j in range(number_of_columns):
+            if horizontal[i][j] == '#' or horizontal[i][j] != '0':
+                out_file_object.write(vertical[i][j])
+            else:
+                out_file_object.write(str(assignment["X"+str(i)+","+str(j)]))
+            if j != number_of_columns - 1:
+                    out_file_object.write(",")
+        out_file_object.write("\n")
+    out_file_object.close()
+    
+def main():
+    input_file_name = input("Enter the path of the kakuro puzzle file: ")
+    input_file_object = open(input_file_name, "r")
+    output_file_name = input("Enter the path of the output file in which answer will be displayed: ")
+    input_file_data = input_file_object.read().splitlines()
+    input_file_object.close()
+    
+    number_of_rows = get_decimal_number(input_file_data[0])
+    number_of_columns = get_decimal_number(input_file_data[1])
+    horizontal = [input_file_data[i].split(",") for i in range(3,3+number_of_rows)]
+    vertical = [input_file_data[i].split(",") for i in range(4+number_of_rows, 4+2*number_of_rows)]
+    csp = make_kakuro_csp(number_of_rows,number_of_columns,horizontal,vertical)
+    #apply arc consistency on all arcs
+    AC_3(csp)
+    #Find the solution by backtracking search
+    assignment = backtracking_search(csp)
+    #print("backtracking calls",backtracking_count)
+    #print("consistency checks by backtracking",consistency_checks_by_backtracking)
+    if assignment == None:
+        print("No solution")
+    else:
+        write_solution_to_output_file(output_file_name,input_file_data,horizontal,vertical,number_of_rows,number_of_columns,assignment)
+        
+if __name__ == "__main__":
+    main()
 
 
 
